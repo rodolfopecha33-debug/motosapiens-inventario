43743c9611a0f8c0fda6c57b176543d225e90d1c
rodolfopecha33-debug <rodolfo.pecha33@gmail.com>
2026-05-08 14:49:55 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,10 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {
+  getStorage
+} from "firebase/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyC8lxiaPtgBD3aaK2TjgO3-yBTEhkwVNK8",
@@ -17,3 +21,6 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export default app;
+export const storage =
+  getStorage(app);
+
