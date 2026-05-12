fcc19c77a9b668517acb4e45b65196b0108054a5
rodolfopecha33-debug <rodolfo.pecha33@gmail.com>
2026-05-11 21:18:24 -0500

    Update Devoluciones.jsx

--- src/Devoluciones.jsx
+++ src/Devoluciones.jsx
@@ -694,45 +694,10 @@ export default function Devoluciones({ user }) {
 
 }
 
-
-            <select
-  className="
-  devolucion-select
-  "
-  value={tipoResolucion}
-  onChange={(e)=>
-
-    setTipoResolucion(
-      e.target.value
-    )
-
-  }
->
-
-  <option value="cambio">
-    Cambio producto
-  </option>
-
-  <option value="dinero">
-    Devolución dinero
-  </option>
-
-  <option value="saldo">
-    Saldo a favor
-  </option>
-
-</select>
+    
 
          
-            <option value="garantia">
-              Garantía
-            </option>
-
-            <option value="insatisfaccion">
-              Insatisfacción Cliente
-            </option>
-
-          </select>
+           
 
         </div>
 
